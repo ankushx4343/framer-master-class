995370cb98a6df2a9ccf8e9fa9991a20c6008a35
Ankush Rana <ankushrana43333@gmail.com>
2025-03-03 00:17:10 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {motion} from 'motion/react';
 const App = () => {
   return (
-    <div className='text-4xl '>
-      hello
+    <div className='text-4xl  h-screen'>
+    <motion.div
+    whileTap={{
+      scale:0.7
+    }}
+    drag
+    className='bg-amber-900 h-[300px] w-[300px] border-6 border-amber-100'>
+    </motion.div>
+    
     </div>
   )
 }
